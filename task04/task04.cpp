c912db080b891531ce02e5fe068c4148599e883e
SurovaSofia <svsurova@edu.hse.ru>
2020-11-29 18:00:45 +0300

    divided into methods

--- task04/task04.cpp
+++ task04/task04.cpp
@@ -19,24 +19,116 @@ int correct_input()
 	return input;
 }
 
+int const num_treads = 3; //количество потоков
+int tread_num; //номер потока
+queue<int> stock; //склад
+omp_lock_t stock_lock;
+queue<int> lorry; //грузовик
+omp_lock_t lorry_lock;
+unsigned long sum; //сумма награбленного
+
+/// <summary>
+/// Симулирует работу прапорщика Иванова
+/// </summary>
+/// <param name="num_items"></param>
+void Ivanov(int &num_items)
+{
+	int count = 0;
+	while (count < num_items)
+	{
+		int temp = rand();
+
+		omp_set_lock(&stock_lock);
+		stock.push(temp);
+
+		#pragma omp critical (output)
+		{
+			cout << "Ivanov took box " << temp << " from the stock." << endl;
+		}
+		omp_unset_lock(&stock_lock);
+
+		count++;
+
+		Sleep(num_items % 131);
+	}
+}
+
+/// <summary>
+/// Симулирует работу прапорщика Петрова
+/// </summary>
+/// <param name="num_items"></param>
+void Petrov(int &num_items)
+{
+	int count = 0;
+	while (count < num_items)
+	{
+		if (!stock.empty())
+		{
+			omp_set_lock(&stock_lock);
+			int temp = stock.front();
+			stock.pop();
+			omp_unset_lock(&stock_lock);
+
+			omp_set_lock(&lorry_lock);
+			lorry.push(temp);
+
+			#pragma omp critical (output)
+			{
+				cout << "Petrov put box " << temp << " in the lorry." << endl;
+			}
+			omp_unset_lock(&lorry_lock);
+
+			count++;
+
+			Sleep(num_items % 131);
+		}
+	}
+}
+
+/// <summary>
+/// Симулирует работу прапорщика Нечепочрук
+/// </summary>
+/// <param name="num_items"></param>
+void Nechepochruk(int &num_items)
+{
+	int count = 0;
+	while (count < num_items)
+	{
+		if (!lorry.empty())
+		{
+			omp_set_lock(&lorry_lock);
+			int temp = lorry.front();
+			lorry.pop();
+
+			sum += temp;
+
+			#pragma omp critical (output)
+			{
+				cout << "Necheporchuk calculated box " << temp << " and got sum " << sum << "." << endl;
+			}
+			omp_unset_lock(&lorry_lock);
+
+			count++;
+
+			Sleep(num_items % 131);
+		}
+	}
+}
+
 int main()
 {
+	//Установка значения для генератора рандомных чисел
+	srand(static_cast<unsigned int>(time(0)));
+
 	//Инициализация переменных
-	int const num_treads = 3; //количество потоков
 	omp_set_num_threads(num_treads);
 
-	int tread_num; //номер потока
-
-	queue<int> stock = queue<int>(); //склад
-	omp_lock_t stock_lock;
+	stock = queue<int>();
 	omp_init_lock(&stock_lock);
-	queue<int> lorry = queue<int>(); //грузовик
-	omp_lock_t lorry_lock;
+	lorry = queue<int>();
 	omp_init_lock(&lorry_lock);
 
-	srand(static_cast<unsigned int>(time(0))); //установка значения для генератора рандомных чисел
-
-	unsigned long sum = 0; //сумма награбленного
+	sum = 0;
 
 	//Ввод количества ящиков
 	cout << "Enter the number of items: ";
@@ -48,76 +140,15 @@ int main()
 		tread_num = omp_get_thread_num();
 		if (tread_num == 0) //Иванов
 		{
-			int count = 0;
-			while (count < num_items)
-			{
-				int temp = rand();
-
-				omp_set_lock(&stock_lock);
-				stock.push(temp);
-
-				#pragma omp critical (output)
-				{
-					cout << "Ivanov took box " << temp << " from the stock." << endl;
-				}
-				omp_unset_lock(&stock_lock);
-
-				count++;
-
-				Sleep(num_items % 131);
-			}
+			Ivanov(num_items);
 		}
 		else if (tread_num == 1) //Петров
 		{
-			int count = 0;
-			while (count < num_items)
-			{
-				if (!stock.empty())
-				{
-					omp_set_lock(&stock_lock);
-					int temp = stock.front();
-					stock.pop();
-					omp_unset_lock(&stock_lock);
-
-					omp_set_lock(&lorry_lock);
-					lorry.push(temp);
-
-					#pragma omp critical (output)
-					{
-						cout << "Petrov put box " << temp << " in the lorry." << endl;
-					}
-					omp_unset_lock(&lorry_lock);
-
-					count++;
-
-					Sleep(num_items % 131);
-				}
-			}
+			Petrov(num_items);
 		}
 		else //Нечепорчук
 		{
-			int count = 0;
-			while (count < num_items)
-			{
-				if (!lorry.empty())
-				{
-					omp_set_lock(&lorry_lock);
-					int temp = lorry.front();
-					lorry.pop();
-
-					sum += temp;
-
-					#pragma omp critical (output)
-					{
-						cout << "Necheporchuk calculated box " << temp << " and got sum " << sum << "." << endl;
-					}
-					omp_unset_lock(&lorry_lock);
-
-					count++;
-
-					Sleep(num_items % 131);
-				}
-			}
+			Nechepochruk(num_items);
 		}
 		#pragma omp barrier 
 	}
